b92f0a557de150f164e56b2419501e0dfddf7a44
MYCOMPUTER\K018A1452 <daikiiijima726@gmail.com>
2021-08-08 16:36:27 +0900

    お金(の元)を作成

---
+++ 5-han/Assets/Script/Money.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Money : MonoBehaviour
+{
+    //プロパティ
+    int amount;//金額
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(amount == 0)
+        {
+            SetAount(20);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetAount(int amount)
+    {
+        this.amount = amount;
+    }
+
+    public int GetMoney()
+    {
+        return amount;
+    }
+}
