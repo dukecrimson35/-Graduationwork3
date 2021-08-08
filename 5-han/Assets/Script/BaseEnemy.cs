c2acaa31c2e1882d02d52725d1de0145253e1fde
takatonzzz <hzmtakaton@gmail.com>
2021-08-08 16:38:49 +0900

    hpの実装 Wでダメージ Qで敵を消す処理

---
+++ 5-han/Assets/Script/BaseEnemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseEnemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int baseEnemyHp;
+    void Start()
+    {
+        baseEnemyHp = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            baseEnemyHp--;
+        }
+        if (baseEnemyHp <= 0)
+        {
+            Destroy(gameObject);
+        }
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
