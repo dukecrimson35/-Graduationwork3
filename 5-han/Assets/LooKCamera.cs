3ce6ceb03466d675125d2875915337a2a77bb2ff
LAPTOP-CT2SJ4KH\machi <machida.1331@gmail.com>
2021-08-08 16:23:31 +0900

    基本動作プレイヤー　カメラ

---
+++ 5-han/Assets/LooKCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LooKCamera : MonoBehaviour
+{
+    public GameObject target;
+    Vector3 length;
+    // Start is called before the first frame update
+    void Start()
+    {
+        length = new Vector3(0, 0, -30);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = length + target.transform.position;
+        transform.LookAt(target.transform);
+     
+    }
+}
---
+++ 5-han/Assets/PlayerControl.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    Vector3 velocity;
+    Vector3 pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocity = Vector3.zero;
+        pos = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    private void Move()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        { 
+            pos.x -= 0.1f;  
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            pos.x += 0.1f;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            velocity = new Vector3(0, 0.5f, 0);
+        }
+
+        if (Mathf.Abs(velocity.y) <= 0.1f)
+        {
+            velocity.y = 0;
+        }
+        else if (velocity.y > 0.1f)
+        {
+            velocity.y -= 0.1f;
+        }
+        else if (velocity.y < -0.1f)
+        {
+            velocity.y += 0.1f;
+        }
+        pos += velocity;
+        //   velocity = Vector3.zero;
+        transform.position += pos;
+        pos = Vector3.zero;
+    }
+}
