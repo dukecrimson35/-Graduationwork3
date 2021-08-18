a23c221475d96ccea106355e57bfa60ab7bdd642
LAPTOP-CT2SJ4KH\machi <machida.1331@gmail.com>
2021-08-18 20:07:48 +0900

    閃空切り少し

---
+++ 5-han/Assets/SenkuSprict.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SenkuSprict : MonoBehaviour
+{
+    float life = 0.2f;
+    // Start is caled before the first frame update
+    void Start()
+    {
+        life = 0.2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        life -= Time.deltaTime;
+        if (life <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
