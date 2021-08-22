33200d56a86d17deac350e389a87fe5a758585ff
takatonzzz <hzmtakaton@gmail.com>
2021-08-22 14:53:03 +0900

    HPをインスペクターから設定可能・仮の点滅処理の作成

--- 5-han/Assets/Script/BaseEnemy.cs
+++ 5-han/Assets/Script/BaseEnemy.cs
@@ -6,17 +6,25 @@ public class BaseEnemy : MonoBehaviour
 {
     // Start is called before the first frame update
     public int baseEnemyHp;
+    private float nextTime;
+    float damageInterval = 1f;
+    bool damage;
+    int count=0;
+    Renderer renderer;
     void Start()
     {
-        baseEnemyHp = 2;
+        damage = false;
+        nextTime = Time.time;
+        renderer = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W))
         {
             baseEnemyHp--;
+            damage = true;
         }
         if (baseEnemyHp <= 0)
         {
@@ -25,6 +33,21 @@ public class BaseEnemy : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Q))
         {
             Destroy(gameObject);
+        }
+        if (damage)
+        {
+            if (Time.time > nextTime)
+            {
+                renderer.enabled = !renderer.enabled;
+
+                nextTime += damageInterval;
+                count++;
+            }
+            if(count==4)
+            {
+                damage = false;
+                count = 0;
+            }
         }
     }
 }
