4951a5e6e5c1bd462e11077d15bebf6004af6109
LAPTOP-CT2SJ4KH\machi <machida.1331@gmail.com>
2021-08-28 06:16:32 +0900

    閃空切り指定方向に床があると動けないように レイヤーにプレイヤー、ブロック追加

--- 5-han/Assets/Script/PlayerControl.cs
+++ 5-han/Assets/Script/PlayerControl.cs
@@ -6,6 +6,8 @@ public class PlayerControl : MonoBehaviour
 {
     Vector3 velocity;
     Vector3 pos;
+    GameObject moveColider;
+    SenkuMove move;
     enum Direc
     {
         Right,Left,
@@ -14,11 +16,15 @@ public class PlayerControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        moveColider = null;
         currentDirec = Direc.Right;
         velocity = Vector3.zero;
         pos = Vector3.zero;
     }
-
+    private void OnCollisionEnter(Collision collision)
+    {
+        int a = 0;
+    }
     // Update is called once per frame
     void Update()
     {
@@ -76,10 +82,29 @@ public class PlayerControl : MonoBehaviour
     {
         if (Input.GetButtonDown("A"))
         {
+            if (moveColider == null)
+            {
+                moveColider = Instantiate((GameObject)Resources.Load("MoveCollider"));
+                
+                move = moveColider.GetComponent<SenkuMove>();
+               // Instantiate(moveColider);
+
+            }
+
+        }
+        if (Input.GetButtonUp("A")&&move.GetIsMove()) 
+        {
             Vector3 senku = new Vector3(Input.GetAxis("Horizontal") * 7, -Input.GetAxis("Vertical") * 7, 0);
             transform.position += senku;
-          
+            Destroy(moveColider);
         }
-        
+        float tes = Mathf.Atan2(-Input.GetAxis("Vertical"), Input.GetAxis("Horizontal")) * 180 / Mathf.PI + 180;
+        Debug.Log(tes);
+        if (moveColider != null)
+        {
+            moveColider.transform.position = transform.position;
+            moveColider.transform.rotation = Quaternion.Euler(0, 0, tes);
+        }
+
     }
 }
---
+++ 5-han/Assets/SenkuMove.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SenkuMove : MonoBehaviour
+{
+    bool isMove = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      //  isMove = true;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Block")
+        {
+            isMove = false;
+        }
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Block")
+        {
+            isMove = false;
+        }
+
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Block")
+        {
+            isMove = true;
+        }
+    }
+    public bool GetIsMove()
+    {
+        return isMove;
+    }
+    public void Dead()
+    {
+        Destroy(gameObject);
+    }
+}
