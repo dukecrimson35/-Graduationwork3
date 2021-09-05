4dc4476dcfdff205764600a020ce5b04a5c09be4
LAPTOP-CT2SJ4KH\machi <machida.1331@gmail.com>
2021-09-05 14:40:50 +0900

    ステータス情報基礎

---
+++ 5-han/Assets/PlayerState.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState : MonoBehaviour
+{
+    int item;
+    int hp = 100;
+    bool deadFlag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DeadCheck();
+    }
+
+    void DeadCheck()
+    {
+        if (hp <= 0)
+        {
+            deadFlag = true;
+        }
+        else
+        {
+            deadFlag = false;
+        }
+    }
+
+    public void Damage(int damage)
+    {
+        hp -= damage;
+    }
+    public bool GetDeadFlag()
+    {
+        return deadFlag;
+    }
+}
