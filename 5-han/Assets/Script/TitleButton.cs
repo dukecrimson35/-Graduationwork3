65f50bc73d05370cccb02b5d83b6f3b9f16745fe
sugahararyota <s.ryota072@gmail.com>
2021-09-08 20:50:22 +0900

    タイトルシーンにボタン追加

---
+++ 5-han/Assets/Script/TitleButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleButton : MonoBehaviour
+{
+
+    public Button startButton;
+    public Button optionButton;
+    public Button endButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startButton.Select();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
