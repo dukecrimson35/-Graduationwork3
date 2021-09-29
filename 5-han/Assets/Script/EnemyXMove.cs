bdee12d28ba1a49fd3a7cd815d83d88a14e7648d
takatonzzz <hzmtakaton@gmail.com>
2021-09-29 12:32:28 +0900

    左右移動敵・上下移動敵(仮)の実装

---
+++ 5-han/Assets/Script/EnemyXMove.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyXMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int baseEnemyHp = 10;
+    private float nextTime;
+    float damageInterval = 1f;
+    bool damage;
+    int count = 0;
+    Renderer renderer;
+    bool LMove;
+    bool RMove;
+    Vector3 pos;
+    void Start()
+    {
+        damage = false;
+        nextTime = Time.time;
+        renderer = GetComponent<Renderer>();
+        LMove = true;
+        RMove = false;
+        pos = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (LMove&&!RMove)
+        {
+            pos.x -= 0.01f;
+        }
+        if(!LMove&&RMove)
+        {
+            pos.x += 0.01f;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            baseEnemyHp--;
+            damage = true;
+        }
+        if (baseEnemyHp <= 0)
+        {
+            Destroy(gameObject);
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Destroy(gameObject);
+        }
+        if (damage)
+        {
+            if (Time.time > nextTime)
+            {
+                renderer.enabled = !renderer.enabled;
+
+                nextTime += damageInterval;
+                count++;
+            }
+            if (count == 4)
+            {
+                damage = false;
+                count = 0;
+            }
+        }
+        transform.position += pos;
+        pos = Vector3.zero;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "SenkuGiri")
+        {
+            baseEnemyHp -= 10;
+        }
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "LSide")
+        {
+            LMove = false;
+            RMove = true;
+        }
+        if (collision.gameObject.tag == "RSide")
+        {
+            LMove = true;
+            RMove = false;
+        }
+    }
+}
---
+++ 5-han/Assets/Script/EnemyYMove.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyYMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int baseEnemyHp = 10;
+    private float nextTime;
+    float damageInterval = 1f;
+    bool damage;
+    int count = 0;
+    Renderer renderer;
+    float nowPosY;
+    void Start()
+    {
+        damage = false;
+        nextTime = Time.time;
+        renderer = GetComponent<Renderer>();
+        nowPosY = this.transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //PingPong関数:指定した振幅や周期で往復させる
+        //PingPong(時間,値) 値は-値にすることはできない
+        //今の位置から＋値の数値まで行って往復してくる処理
+        transform.position = new Vector3(transform.position.x, nowPosY + Mathf.PingPong(Time.time / 2, 3), transform.position.z);
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            baseEnemyHp--;
+            damage = true;
+        }
+        if (baseEnemyHp <= 0)
+        {
+            Destroy(gameObject);
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Destroy(gameObject);
+        }
+        if (damage)
+        {
+            if (Time.time > nextTime)
+            {
+                renderer.enabled = !renderer.enabled;
+
+                nextTime += damageInterval;
+                count++;
+            }
+            if (count == 4)
+            {
+                damage = false;
+                count = 0;
+            }
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "SenkuGiri")
+        {
+            baseEnemyHp -= 10;
+        }
+
+    }
+}
