4a972590a09720b7e102284f3324e1d97b23cc0e
sugahararyota <s.ryota072@gmail.com>
2021-10-23 12:38:58 +0900

    ポーズメニューのバグ（プレイヤーが動けちゃう）修正

--- 5-han/Assets/Resources/Prefabs/UI/PauseSoundScript.cs
+++ 5-han/Assets/Resources/Prefabs/UI/PauseSoundScript.cs
@@ -162,7 +162,7 @@ public class PauseSoundScript : MonoBehaviour
         {
             Data.pauseWindFlag = false;
             Destroy(this.gameObject.transform.parent.parent.gameObject);
-            Time.timeScale = 1f;
+            //Time.timeScale = 1f;
             return;
         }
 
