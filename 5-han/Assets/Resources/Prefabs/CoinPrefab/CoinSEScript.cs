d42c7972b36a02e0035d2fdf87dbde5ee32035c4
sugahararyota <s.ryota072@gmail.com>
2021-11-08 09:19:16 +0900

    合わせるためのコミット

---
+++ 5-han/Assets/Resources/Prefabs/CoinPrefab/CoinSEScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSEScript : MonoBehaviour
+{
+    public List<bool> seFlagList = new List<bool>();
+
+    public List<int> timeList = new List<int>();
+
+    private bool[] seFlags = new bool[10];
+
+
+    public AudioSource audioSource;
+    public AudioClip se;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlaySE()
+    {
+        audioSource.PlayOneShot(se);
+        seFlagList.Add(true);
+
+    }
+}
