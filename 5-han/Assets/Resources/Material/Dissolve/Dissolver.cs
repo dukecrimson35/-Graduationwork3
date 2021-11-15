09bcc21a2d808940b0c6cf70296037e6c7bab6d4
sugahararyota <s.ryota072@gmail.com>
2021-11-15 09:30:52 +0900

    飛んでるコインに当たって宝箱が動くバグ修正

---
+++ 5-han/Assets/Resources/Material/Dissolve/Dissolver.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Renderer))]
+public class Dissolver : MonoBehaviour
+{
+
+    Material m_Material;
+    YieldInstruction m_Instruction = new WaitForEndOfFrame();
+
+    void Start()
+    {
+        m_Material = GetComponent<Renderer>().material;
+        StartCoroutine("Animate");
+    }
+
+    IEnumerator Animate()
+    {
+        float time = 0;
+        float duration = 2f;
+        int dir = 1;
+
+        while (true)
+        {
+            yield return m_Instruction;
+
+            time += Time.deltaTime * dir;
+            var t = time / duration;
+
+            if (t > 1f)
+            {
+                dir = -1;
+            }
+            else if (t < 0)
+            {
+                dir = 1;
+            }
+
+            m_Material.SetFloat("_CutOff", t);
+        }
+    }
+}
