63caf310b73e3c7b940c4ac386346a0af91262be
sugahararyota <s.ryota072@gmail.com>
2021-11-15 15:52:15 +0900

    Coinsプレハブがずっと残ってたバグなおした

---
+++ 5-han/Assets/Resources/Prefabs/CoinPrefab/CoinsDestroy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinsDestroy : MonoBehaviour
+{
+
+    float count = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        count += Time.deltaTime;
+
+        if(count> 5)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
