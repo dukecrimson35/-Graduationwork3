96fca907d1972eb3405ef825d69c06504cd1b0d8
MYCOMPUTER\K018A1452 <daikiiijima726@gmail.com>
2021-11-19 12:07:59 +0900

    閃空斬りの判定をisTriggerに

--- 5-han/Assets/SenkuSprict.cs
+++ 5-han/Assets/SenkuSprict.cs
@@ -61,12 +61,44 @@ public class SenkuSprict : MonoBehaviour
             Destroy(collision.gameObject);
         }
     }
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider collision)
     {
-        if (other.transform.tag == "Bullet")
+        if (collision.transform.tag == "Enemy")
         {
             GameObject after = Instantiate((GameObject)Resources.Load("SenkuEffect"));
-            after.transform.position = other.transform.position;
+            after.transform.position = collision.transform.position;
+            float ang = Mathf.Atan2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal")) * 180 / Mathf.PI;
+            after.transform.rotation = Quaternion.Euler(0, 0, ang);
+            hit = true;
+        }
+        if (collision.transform.tag == "Bullet")
+        {
+            GameObject after = Instantiate((GameObject)Resources.Load("SenkuEffect"));
+            after.transform.position = collision.transform.position;
+            float ang = Mathf.Atan2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal")) * 180 / Mathf.PI;
+            after.transform.rotation = Quaternion.Euler(0, 0, ang);
+            hit = true;
+        }
+        if (collision.transform.tag == "BossEnemy")
+        {
+            GameObject after = Instantiate((GameObject)Resources.Load("SenkuEffect"));
+            after.transform.position = collision.transform.position;
+            float ang = Mathf.Atan2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal")) * 180 / Mathf.PI;
+            after.transform.rotation = Quaternion.Euler(0, 0, ang);
+
+            hit = true;
+        }
+
+        if (collision.gameObject.tag == "Coin")
+        {
+            Money moneyScript = collision.gameObject.GetComponent<Money>();
+            itemManagerScript.UpCoin(moneyScript.GetMoney());
+            Destroy(collision.gameObject);
+        }
+        if (collision.transform.tag == "Bullet")
+        {
+            GameObject after = Instantiate((GameObject)Resources.Load("SenkuEffect"));
+            after.transform.position = collision.transform.position;
             float ang = Mathf.Atan2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal")) * 180 / Mathf.PI;
             after.transform.rotation = Quaternion.Euler(0, 0, ang);
             hit = true;
