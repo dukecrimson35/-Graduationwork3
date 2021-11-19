4fcb3eb7f75a43eb6137322fac6ac12167a26fb9
MYCOMPUTER\K018A1452 <daikiiijima726@gmail.com>
2021-11-19 12:33:11 +0900

    fps表示して重いか確認できるよ

---
+++ 5-han/Assets/fpsDisplay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class fpsDisplay : MonoBehaviour
+{
+    float fps;
+    public Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fps = 1f / Time.deltaTime;
+        text.text = fps.ToString();
+    }
+}
