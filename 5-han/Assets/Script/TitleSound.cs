026e59924ed931e348ef2fbc1548fba460f0fe8c
sugahararyota <s.ryota072@gmail.com>
2021-11-26 09:31:49 +0900

    ボスの間の名前変更

---
+++ 5-han/Assets/Script/TitleSound.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleSound : MonoBehaviour
+{
+    public GameObject titleBGM;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!GameObject.Find("TitleBGM"))
+        {
+            GameObject instance =
+                    (GameObject)Instantiate(titleBGM,
+                    new Vector3(0, 0, 0.0f), Quaternion.identity);
+        }
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
