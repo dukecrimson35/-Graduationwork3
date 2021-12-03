e4e2bf648a1b8ec42c8bc49c9ae922076d0e10e6
MYCOMPUTER\K018A1452 <daikiiijima726@gmail.com>
2021-12-03 09:58:41 +0900

    九尾ボスの動き途中

---
+++ 5-han/Assets/Script/KyuubiAvatar.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KyuubiAvatar : MonoBehaviour
+{
+    public float speed;//通常時の移動速度
+    public GameObject Texture;//自分の画像
+    GameObject player;
+    Vector3 playerPos;
+    bool leftMove;//左右どちらに移動するか
+    bool onGround;//地面にいるかどうか
+
+    Rigidbody rigidbody;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerPos = player.transform.position;//プレイヤーのポジション取得
+        if (leftMove)//左に移動
+        {
+            rigidbody.velocity = new Vector3(-Mathf.Pow(speed, 2) * 1.75f, rigidbody.velocity.y, 0);
+            Texture.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+            if (transform.position.x - playerPos.x <= -5)
+            {
+                leftMove = false;
+                speed = 1;
+            }
+        }
+        if (leftMove == false)//右に移動
+        {
+            rigidbody.velocity = new Vector3(Mathf.Pow(speed, 2) * 1.75f, rigidbody.velocity.y, 0);
+            Texture.transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
+            if (transform.position.x - playerPos.x >= 5)
+            {
+                leftMove = true;
+                speed = 1;
+            }
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Block")
+        {
+            onGround = false;
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Block")//地面と衝突したとき
+        {
+            onGround = true;
+        }
+    }
+}
